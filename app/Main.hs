249643278437bb94e5c89308a253b48213459130
Evgeny Petrovsky <evgeny.petrovsky@ing.com>
2024-12-10 22:17:31 +0100

    more formatting

--- app/Main.hs
+++ app/Main.hs
@@ -1,23 +1,14 @@
 module Main (main) where
 
-import Lib
 import qualified Day10 as D
-
---import Lib
-
+import Lib
 import Util
 
 main :: IO ()
 main = do
-    -- putStrLn $ greet "World"
 
-    input <- getContents
-    let problem = D.Input input :: D.Problem
-    -- let answer1 = solve1 problem
+  input <- getContents
+  let problem = D.Input input :: D.Problem
 
-    -- let problem = parse1 input :: Problem
-    -- let part1s = solve1 problem
-    -- let part2s = solve2 problem
-
-    putStrLn $ "Part 1: " ++ (problem |> solveShow1)
-    putStrLn $ "Part 2: " ++ (problem |> solveShow2)
+  putStrLn $ "Part 1: " ++ (problem |> solveShow1)
+  putStrLn $ "Part 2: " ++ (problem |> solveShow2)
--- src/Util.hs
+++ src/Util.hs
@@ -1,15 +1,17 @@
-module Util ( 
-  (|>)
-, stringToInts
-, frequency
-, distinct
-) where
+module Util
+  ( (|>),
+    stringToInts,
+    frequency,
+    distinct,
+  )
+where
 
-import qualified Data.Map.Strict as Map (empty, insertWith, Map)
+import qualified Data.Map.Strict as Map (Map, empty, insertWith)
 import qualified Data.Set as Set (fromList, toList)
 
 (|>) :: a -> (a -> b) -> b
 a |> f = f a
+
 infixl 9 |>
 
 stringToInts :: String -> [Int]
@@ -17,9 +19,9 @@ stringToInts s = words s |> map read
 
 frequency :: (Ord a) => [a] -> Map.Map a Int
 frequency = foldl update emptyM
-    where
-      emptyM = Map.empty :: Map.Map a Int
-      update z x = Map.insertWith (+) x 1 z
+  where
+    emptyM = Map.empty :: Map.Map a Int
+    update z x = Map.insertWith (+) x 1 z
 
-distinct :: Ord a => [a] -> [a]
+distinct :: (Ord a) => [a] -> [a]
 distinct a = a |> Set.fromList |> Set.toList
